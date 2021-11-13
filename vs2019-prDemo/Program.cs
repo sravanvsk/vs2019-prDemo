411686b6ea35a2b72ababbac004f6a206681f396
sravanvsk <vorugantisravan7@gmail.com>
2021-11-13 09:10:40 +0530

    Add project files.

---
+++ vs2019-prDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vs2019_prDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
