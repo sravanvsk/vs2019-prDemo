6c1910f3aa39b9dedd73be0d20720150f06cf2f0
ramkuumar <ram.ramkumar150@gmail.com>
2021-11-13 09:34:48 +0530

    apples

--- vs2019-prDemo/Program.cs
+++ vs2019-prDemo/Program.cs
@@ -7,6 +7,7 @@ namespace vs2019_prDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Apples");
         }
     }
 }
